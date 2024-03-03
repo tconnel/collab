3a173856343e7fa05e6caeda3f37a81bdbe63faa
ClevrGrl <96588932+ClevrGrl@users.noreply.github.com>
2024-03-02 19:11:44 -0700

    Create d49adminsecuretoken.sh

---
+++ d49adminsecuretoken.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Prompts the user for administrator password
+adminPassword=$(
+    osascript <<END
+set userInput to the text returned of (display dialog "Please input your Macbook Password:" default answer "" with hidden answer)
+return userInput
+END
+)
+result=$(
+    echo "$adminPassword" | sudo -S ls /var/root 2>&1 >/dev/null
+    echo "$?"
+)
+
+if [[ "$result" != "Password:0" ]]; then
+    osascript -e 'display dialog "Password Incorrect"'
+fi
+
+#Pulls current LAPS password and grants to d49admin
+currentUser=$(id -un)
+LAPSPassword=$(macOSLAPS -getPassword | cat /var/root/Library/Application\ Support/macOSLAPS-password)
+sysadminctl -secureTokenOn -user d49admin -password $LAPSPassword -adminUser $currentUser -adminPassword $adminPassword
