774f9f021739c30d24809f43272c708edcd789fd
Tyler Connel <tyler@schedmd.com>
2024-03-03 15:01:04 -0800

    Add loop for prompting password.
    * `sudo -k` to clear cache.
    * 1 second delay for naive throttle of brute-force attack.
    * Adjusted read command to not not allow escape chararcters.

--- d49adminsecuretoken.sh
+++ d49adminsecuretoken.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
 #Prompts the user for administrator password
-#NOTE bash native 'read' will probably do what you were accomplishing with
-#     osascript and will be more portable across varios OS flavors (works with
-#     any 'bash' implementation consistently).
-read -s -p "Please input your Macbook Password:" adminPassword
+while :; do
 
-result=$(
-    echo "$adminPassword" | sudo -S ls /var/root 2>&1 >/dev/null
-    echo "$?"
-)
+    #NOTE bash native 'read' will probably do what you were accomplishing with
+    #     osascript and will be more portable across varios OS flavors (works with
+    #     any 'bash' implementation consistently, including linux).
+    # -p <prompt> => use the provided 'prompt' as the prompt
+    # -s => do not echo input on stdout (won't show the user's password back to them
+    IFS= read -rs -p "Please input your Macbook Password:" adminPassword
+    echo "" #the -p option of read will not insert a newline/carriage-return
 
-if [[ "$result" != "Password:0" ]]; then
-    osascript -e 'display dialog "Password Incorrect"'
-fi
+    #NOTE it may not be the same on MacOS, but in some linux distros the authentication
+    #     will be cached and the user will not need to be prompted again for a password.
+    #     'sudo -k' destroys any prior cached auth data.
+    sudo -k
+
+    result=$(
+        echo "$adminPassword" | sudo -S ls /var/root &>derp
+        echo "$?"
+    )
+
+    # check result, exit loop if correct password was supplied.
+    # continue loop and prompt again if otherwise.
+    if [[ "$result" = "0" ]]; then
+        break
+    else
+        # prints to stderr (with redirection)
+        echo 'Incorrect Password' >&2
+    fi
+
+    #NOTE not strictly necessary, but a 'tight loop' is *considerably* more vulnerable to
+    #     brute-force attacks. The larger security concern is in testing the plaintext
+    #     password without salt+hash.
+    sleep 1
+
+done
 
 #Pulls current LAPS password and grants to d49admin
 currentUser=$(id -un)
